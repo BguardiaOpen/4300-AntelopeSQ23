2875b034847926c6b527cd2973d5987b93f2e814
bduong02 <65621073+bduong02@users.noreply.github.com>
2023-05-28 02:48:14 -0700

    Update TransactionStatement.h

--- TransactionStatement.h
+++ TransactionStatement.h
@@ -2,13 +2,14 @@
 #define __TRANSACTION_STATEMENT_H__
 
 #include "SQLStatement.h"
+#include "Table.h"
 
 // Note: Implementations of constructors and destructors can be found in statements.cpp.
 namespace hsql {
   // Represents SQL Begin statements.
   // Example "BEGIN TRANSACTION;"
   struct TransactionStatement : SQLStatement {
-    BeginStatement();
+    TransactionStatement(ActionType type);
     virtual ~BeginStatement();
     
     enum ActionType {
